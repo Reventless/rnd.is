20656e79e463fdf492a60ee11d7ec2636c71ecdf
Stephan <stephan.lamersdorf@gmail.com>
2023-04-26 19:23:36 +0200

    fixes

--- src/bahnapicall.rs
+++ src/bahnapicall.rs
@@ -1,4 +1,3 @@
-use reqwest::blocking::Client;
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Debug)]
@@ -23,22 +22,34 @@ struct StationBoard {
     entries: Vec<StationBoardEntry>,
 }
 
-fn main() -> Result<(), reqwest::Error> {
+async fn main() -> Result<(), reqwest::Error> {
     let client = Client::new();
     let station_id = "8000085"; // Düsseldorf Hbf station ID
-    let api_key = "apikey";
+    let api_key = "";
     let url = format!(
         "https://api.deutschebahn.com/fahrplan-plus/v1/arrivalBoard/{station_id}?date={date}",
         station_id = station_id,
         date = chrono::Utc::now().format("%Y-%m-%d").to_string(),
     );
-    let res = client
-        .get(&url)
-        .header("Authorization", format!("Bearer {}", api_key))
-        .header("Accept", "application/json")
-        .send()?
-        .json::<StationBoard>()?;
-    for entry in res.entries {
+    let mut map = HeaderMap::new();
+
+    assert_eq!(0, map.len());
+
+    map.insert(ACCEPT, "text/plain".parse().unwrap());
+    map.insert(HOST, "localhost".parse().unwrap());
+
+    assert_eq!(2, map.len());
+
+    map.append(ACCEPT, "text/html".parse().unwrap());
+
+    assert_eq!(3, map.len());
+    let res = reqwest::get(url)
+        .await?
+        .headers("Authorization", format!("Bearer {}", api_key))
+        .headers("Accept", "application/json")
+        .json::<StationBoard>()?
+        .await?;
+        for entry in res.entries {
         println!(
             "Train {} ({}) at {} is expected to arrive at {} with a delay of {} minutes",
             entry.name,
